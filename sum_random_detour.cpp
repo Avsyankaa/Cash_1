a8277c88a2950d02b0fe84055444941110d74a76
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-07 15:32:46 +0300

    Create sum_random_detour.cpp

---
+++ sum_random_detour.cpp
@@ -0,0 +1,9 @@
+#include "detours.hpp"
+long sum_random_detour(int * array, int capacity) {
+	long sum = 0;
+	while (capacity != 0) {
+		int delete_element = rand() % capacity; // тут мы получили элемент вектора
+		sum += array[delete_element];
+		std::swap(array[delete_element], array[capacity-1]);
+		capacity--;
+	}
