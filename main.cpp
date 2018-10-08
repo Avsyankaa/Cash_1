c098108ad567de22d3a1cc77230d167021abd7d7
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-08 19:21:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
+#include <chrono>
+#include <fstream>
 #include "detours.hpp"
 int main() {
-std::ofstream file;
-file.open("Avsyanochka.txt");
-unsigned capacity = 4096;
-while (capacity !=35389440) {
-unsigned* array = new unsigned[capacity];
+	std::ofstream file;
+	file.open("Avsyanochka.txt");
+	unsigned capacity = 4096;
+	while (capacity != 35389440) {
+		unsigned* array = new unsigned[capacity];
 		for (unsigned i = 0; i < capacity; i++) {
 			array[i] = rand() % 3;
 		}
 		sum_straight_detour(array, capacity);
-	    	auto start_time = std::chrono::high_resolution_clock::now();
+		auto start_time = std::chrono::high_resolution_clock::now();
 		for (unsigned i = 0; i < 1000; i++)
-		sum_straight_detour(array, capacity);
+			sum_straight_detour(array, capacity);
 		auto end_time = std::chrono::high_resolution_clock::now();
 		auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-		file<<"count_elements=" << capacity << " " << "time:" << diff << " " << "straight detour" << std::endl;
+		file << "count_elements=" << capacity << " " << "time:" << diff << " " << "straight detour" << std::endl;
 		file << std::endl;
 		delete[] array;
 		unsigned* array1 = new unsigned[capacity];
@@ -24,11 +26,11 @@ unsigned* array = new unsigned[capacity];
 		sum_reverse_detour(array1, capacity);
 		start_time = std::chrono::high_resolution_clock::now();
 		for (unsigned i = 0; i < 1000; i++)
-		sum_reverse_detour(array1, capacity);
+			sum_reverse_detour(array1, capacity);
 		end_time = std::chrono::high_resolution_clock::now();
 		diff = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-		file<<"count_elements="<<capacity<<" "<< "time:"<<diff<<" "<<"reverse detour"<<std::endl;
-		file<<std::endl;
+		file << "count_elements=" << capacity << " " << "time:" << diff << " " << "reverse detour" << std::endl;
+		file << std::endl;
 		delete[] array1;
 		unsigned* array2 = new unsigned[capacity];
 		for (unsigned i = 0; i < capacity; i++) {
@@ -37,11 +39,11 @@ unsigned* array = new unsigned[capacity];
 		sum_random_detour(array2, capacity);
 		start_time = std::chrono::high_resolution_clock::now();
 		for (unsigned i = 0; i < 1000; i++)
-		sum_random_detour(array2, capacity);
+			sum_random_detour(array2, capacity);
 		end_time = std::chrono::high_resolution_clock::now();
 		diff = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-		file<<"count_elements="<<capacity<<" "<< "time:"<<diff<<" "<<"random detour"<<std::endl;
-		file<<std::endl;
+		file << "count_elements=" << capacity << " " << "time:" << diff << " " << "random detour" << std::endl;
+		file << std::endl;
 		delete[] array2;
 		if (capacity == 16777216) capacity = 17694720;
 		else capacity = capacity * 2;
