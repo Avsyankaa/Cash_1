8293ee031133a532b5a716c2f98c8997416952ad
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-08 19:23:58 +0300

    Update sum_reverse_detour.cpp

--- sum_reverse_detour.cpp
+++ sum_reverse_detour.cpp
@@ -1,7 +1,7 @@
 #include "detours.hpp"
-long sum_reverse_detour (int *&array, int capacity) {
-	long sum = 0;
-	for (int i = capacity-1; i > -1; i--)
+unsigned long sum_reverse_detour(unsigned*& array, unsigned capacity) {
+	unsigned long sum = 0;
+	for (unsigned i = capacity - 1; i > -1; i--)
 		sum += array[i];
 	return sum;
 }
