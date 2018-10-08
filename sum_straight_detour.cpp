9d587f9382f85c81d6c3531f0b89e0e1732a9ea8
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-08 19:24:29 +0300

    Update sum_straight_detour.cpp

--- sum_straight_detour.cpp
+++ sum_straight_detour.cpp
@@ -1,7 +1,7 @@
-#include "detours.hpp" 
-long sum_straight_detour(int *&array, int capacity) {
-	long sum=0;
-	for (int i = 0; i < capacity; i++)
+#include "detours.hpp"
+unsigned long sum_straight_detour(unsigned*& array, unsigned capacity) {
+	unsigned long sum = 0;
+	for (unsigned i = 0; i < capacity; i++)
 		sum += array[i];
 	return sum;
 }
