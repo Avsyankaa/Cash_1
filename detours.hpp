ce60c27915f2b46a30469c9fef25564210ce476d
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-08 19:25:10 +0300

    Update detours.hpp

--- detours.hpp
+++ detours.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
-long sum_straight_detour(int *&array, int capacity);
-long sum_reverse_detour (int *&array, int capacity);
-long sum_random_detour(int * array, int capacity);
+unsigned long sum_straight_detour(unsigned*& array, unsigned capacity);
+unsigned long sum_reverse_detour (unsigned*& array, unsigned capacity);
+unsigned long sum_random_detour(unsigned* array, unsigned capacity);
+
