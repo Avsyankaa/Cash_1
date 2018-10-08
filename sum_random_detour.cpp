878efb00179aee102516e9788ed20b5124deae98
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-10-08 20:45:02 +0300

    Update sum_random_detour.cpp

--- sum_random_detour.cpp
+++ sum_random_detour.cpp
@@ -2,7 +2,7 @@
 unsigned long sum_random_detour(unsigned* array, unsigned capacity) {
     unsigned long sum = 0;
     while (capacity != 0) {
-        unsigned delete_element = rand() % capacity;
+        unsigned delete_element = std::rand() % capacity;
         sum += array[delete_element];
         std::swap(array[delete_element], array[capacity - 1]);
         capacity--;
